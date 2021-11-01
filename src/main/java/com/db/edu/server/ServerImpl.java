bbeb15fe2a683885c7382241119eed73f16c219f
Arsenii Marshinin <marshinin.ae@phystech.edu>
2021-11-01 10:37:16 +0300

    ServerImpl added

---
+++ src/main/java/com/db/edu/server/ServerImpl.java
@@ -0,0 +1,13 @@
+package com.db.edu.server;
+
+public class ServerImpl implements Server{
+    @Override
+    public void getRequest(String message) {
+
+    }
+
+    @Override
+    public String sendResponse() {
+        return null;
+    }
+}
