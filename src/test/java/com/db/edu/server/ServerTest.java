1d4d1fec5e49d6a120ca1a279eb2e173c146a554
Anna Shipil <anna.shipil@db.com>
2021-11-01 11:08:37 +0300

    Editing the test

--- src/test/java/com/db/edu/server/ServerTest.java
+++ src/test/java/com/db/edu/server/ServerTest.java
@@ -1,13 +1,29 @@
 package com.db.edu.server;
 
 import com.db.edu.ServerImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import com.acme.edu.SysoutCaptureAndAssertionAbility;
 
 public class ServerTest implements SysoutCaptureAndAssertionAbility {
+    @BeforeEach
+    public void setUpSystemOut() throws IOException {
+        resetOut();
+        captureSysout();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        resetOut();
+    }
+
     @Test
     public void shouldReturnHistory() throws IOException {
         ServerImpl.getRequest("//snd Hello, world!");
         ServerImpl.getRequest("//hist");
+        ServerImpl.sendResponse();
 
         assertSysoutContains("Hello, world!");
     }
