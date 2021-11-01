2b5ceddd483d6f33b2e1791437777672c70549ac
Anna Shipil <anna.shipil@db.com>
2021-11-01 11:02:38 +0300

    Adding test

--- src/test/java/com/db/edu/server/ServerTest.java
+++ src/test/java/com/db/edu/server/ServerTest.java
@@ -1,4 +1,14 @@
 package com.db.edu.server;
 
-public class ServerTest {
+import com.db.edu.ServerImpl;
+import com.acme.edu.SysoutCaptureAndAssertionAbility;
+
+public class ServerTest implements SysoutCaptureAndAssertionAbility {
+    @Test
+    public void shouldReturnHistory() throws IOException {
+        ServerImpl.getRequest("//snd Hello, world!");
+        ServerImpl.getRequest("//hist");
+
+        assertSysoutContains("Hello, world!");
+    }
 }
