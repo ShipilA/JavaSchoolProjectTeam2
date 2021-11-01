c88d449318e34c393d476bad21306ef2514c2410
Anna Shipil <anna.shipil@db.com>
2021-11-01 11:12:07 +0300

    Added a logger

--- src/main/java/com/db/edu/server/ServerImpl.java
+++ src/main/java/com/db/edu/server/ServerImpl.java
@@ -1,5 +1,7 @@
 package com.db.edu.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.db.edu.server.exception.ServerException;
 import com.db.edu.server.request.Request;
 import com.db.edu.server.request.RequestFactory;
@@ -7,6 +9,7 @@ import com.db.edu.server.request.SendMessageRequest;
 import com.db.edu.server.request.ShowHistoryRequest;
 
 public class ServerImpl implements Server{
+    final Logger logger = LoggerFactory.getLogger(ServerImpl.class);
     @Override
     public void getRequest(String message) {
         RequestFactory requestFactory = new RequestFactory();
