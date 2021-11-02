59087290ff604ec20f5b80d4d1255a659be18fa9
Arsenii Marshinin <marshinin.ae@phystech.edu>
2021-11-02 14:20:42 +0300

    Useless method removed

--- src/main/java/com/db/edu/server/rooms/RoomContainer.java
+++ src/main/java/com/db/edu/server/rooms/RoomContainer.java
@@ -1,16 +1,10 @@
 package com.db.edu.server.rooms;
 
-import com.db.edu.server.exception.ServerException;
-
 import java.util.LinkedList;
 import java.util.List;
 
 public class RoomContainer {
     private final List<Room> rooms = new LinkedList<>();
-
-    public void addRoom(Room room) {
-        rooms.add(room);
-    }
 
     public Room getRoomWithName(String name) {
         for (Room room : rooms) {
