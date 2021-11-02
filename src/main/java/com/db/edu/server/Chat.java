9d938225f22eecff7bfa3f4fb9f3a039624d9853
Vladimir Zarubin <equationlxl@gmail.com>
2021-11-02 11:23:51 +0300

    Logger added (partly)

--- src/main/java/com/db/edu/server/Chat.java
+++ src/main/java/com/db/edu/server/Chat.java
@@ -1,12 +1,15 @@
 package com.db.edu.server;
 
 import com.db.edu.server.rooms.Room;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Chat {
+    static Logger log = LoggerFactory.getLogger(Chat.class);
 
     public static void main(String[] args) {
         try {
@@ -20,7 +23,7 @@ public class Chat {
                 thread.start();
             }
         } catch (IOException e) {
-            //TODO add logger
+            log.error("Failed to run server: {}", e);
         }
     }
 }
